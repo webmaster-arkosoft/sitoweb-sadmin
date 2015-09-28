7df49281ec41b649b569cbebeb138b1cc672028f
webmaster-arkosoft <webmaster@arkosoft.it>
2015-09-28 16:35:26 +0200

    prova font family

--- js/funzioni.js
+++ js/funzioni.js
@@ -1,6 +1,5 @@
-function myFunction(){
+function myZindex(){
 	document.getElementById("cd-main-content").style.zIndex="1";
 	document.getElementById("header").style.zIndex="2";
 	document.getElementById("cd-lateral-nav").style.zIndex="3";
-	document.getElementById(".nojs").style.zIndex="100";
 }
